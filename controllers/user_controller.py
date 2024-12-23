98a0d78d638d286d99e1b476680a5024190b5bd4
Rushit PC <rushitchandani@gmail.com>
2024-12-23 23:29:59 +0530

    validation in signup

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,23 +1,68 @@
 from flask import json
 from models.main_users import main_users
+from libraries.functions import functions
+import hashlib
 
 class user_controller():
+    def __init__(self):
+        self.functionsObj = functions()
 
     def signup(self, request):
 
         username = request.form.get("username", '', str)
         password = request.form.get("password", '', str)
         dob = request.form.get('dob', '', str)
-        mobile = request.form.get('mobile', 0, int)
-        gender = request.form.get('gender', '' , str)
+        mobile = request.form.get('mobile', '', str)
+        gender = request.form.get('gender', '' , str).lower()
         firstname = request.form.get('firstname', '', str)
         lastname = request.form.get('lastname', '', str)
         email = request.form.get("email", '', str)
 
-        # return 0 
+        validation_results = {}
+
+        is_valid, message = self.functionsObj.validate_username(username)
+        validation_results["username"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_password(password)
+        validation_results["password"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_dob(dob)
+        validation_results["dob"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_mobile(mobile)
+        validation_results["mobile"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_gender(gender)
+        validation_results["gender"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_name(firstname)
+        validation_results["firstname"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_name(lastname)
+        validation_results["lastname"] = message if not is_valid else "Valid"
+
+        is_valid, message = self.functionsObj.validate_email(email)
+        validation_results["email"] = message if not is_valid else "Valid"
+
+        for f, result in validation_results.items():
+            print(f, result)
+            if result != "Valid":
+                return self.functionsObj.send_response(0, f"{result}")
+
+        main_usersObj = main_users()
+        main_usersObj.where = f"username = '{username}'"
+        result = main_usersObj.select()
+        if len(result) > 0:
+            return self.functionsObj.send_response(0, "Same user Already exists.")
+        
+        main_usersObj.where = f"mobile = '{mobile}'"
+        result = main_usersObj.select()
+        if len(result) > 0:
+            return self.functionsObj.send_response(0, "Same mobile Already exists.")
+
         insert_dict = {
             "username" : username ,
-            "password" : password,
+            "password" : self.functionsObj.generate_hash(password),
             "dob" : dob,
             "mobile" : mobile,
             "gender" : gender,
@@ -25,9 +70,9 @@ class user_controller():
             "last_name" : lastname,
             "email" : email
         }
-        main_usersObj = main_users()
         inserted_record = main_usersObj.insert('',tuple(insert_dict.keys()), tuple(insert_dict.values()))
-        return {'status_code':0,'status_message':'User Inserted'}
+        if inserted_record > 0:
+            return self.functionsObj.send_response(1, "Registration successfull!")
     
     def login(self, request):
         username = request.form.get("username", '', str)
@@ -39,3 +84,4 @@ class user_controller():
         if len(records) > 0:
             return {'status_code':0,'status_message':'Login DOne'}
         return {'status_code':1,'status_message':'Login Failed'}
+    
---
+++ libraries/functions.py
@@ -0,0 +1,72 @@
+import re
+from datetime import datetime
+import hashlib
+
+class functions():
+    def send_response(self, status_code, message, data = []):
+        return {
+            "status_code" : status_code,
+            "status_message" : message,
+            "data" : data
+        }
+    
+    def validate_username(self, username:str) -> tuple:
+        if not isinstance(username, str) or not str(username).isalnum():
+            return False, "Username must be alphanumeric."
+        if len(username) < 5 or len(username) > 15:
+            return False, "Username must be between 5 to 15 characters."
+        return True, ""
+    
+    def validate_password(self, password:str) -> tuple:
+        if len(password) < 8:
+            return False, "Length of password must be more than 8."
+        if not re.search(r'[A-Z]', password):
+            return False, "Password must contain atleast one uppercase letter."
+        if not re.search(r'[a-z]', password):
+            return False, "Password must contain atleast one lowercase letter."
+        if not re.search(r'[0-9]', password):
+            return False, "Password must contain atleast one numeric letter."
+        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+            return False, "Password must contain atleast one special character."
+        return True, ""
+    
+    def validate_dob(self, dob:str) -> tuple:
+        try:
+            dob_date = datetime.strptime(dob, "%Y-%m-%d")
+        except ValueError:
+            return False, "Date of birth must be in the format YYYY-MM-DD."
+        
+        if dob_date.date() >= datetime.now().date():
+            return False, "Current or future DOB is not acceptable."
+        
+        return True, ""
+    
+    def validate_mobile(self, mobile:str) -> tuple:
+        if not mobile.isdigit():
+            return False, "Mobile number must be digit."
+        if len(mobile) != 10:
+            return False, "Length of mobile number must be 10 digits."
+        return True, ""
+    
+    def validate_gender(self, gender:str) -> tuple:
+        if gender.lower() not in ['male', 'female', 'other']:
+            return False, "Gender must be either 'Male', 'Female', or 'Other'."
+        return True, ""
+    
+    def validate_name(self, name:str) -> tuple:
+        if not name.isalpha():
+            return False, f"{name} must contain only alphabetic characters."
+        if len(name) == 0:
+            return False, "Name cannot be empty."
+        return True, ""
+    
+    def validate_email(self, email:str) -> tuple:
+        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(email_regex, email):
+            return False, "Email must be in a valid format."
+        return True, ""
+    
+    def generate_hash(self, string:str) -> str:
+        sha256 = hashlib.sha256()
+        sha256.update(string.encode('utf-8'))
+        return sha256.hexdigest()[:20]
