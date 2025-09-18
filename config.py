23ea2e4951a33b4b441f20873cf4eef90602425b
Rushit PC <rushitchandani@gmail.com>
2025-09-18 21:39:12 +0530

    config updated

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = "fgh/35t#%2grt"
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://expensetracker_policeman:e2ff141ef5f5771ddda38f3fd762709e9334e510@q0rsi6.h.filess.io:3307/expensetracker_policeman"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://rushnb10_rushit:Rushit@2002@localhost/rushnb10_expensedb"
     # SQLALCHEMY_DATABASE_URI = "sqlite:///expenses.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
