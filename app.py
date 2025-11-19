2bb94025d0a8c1224b3fe03e76c3cfb9496f9ab7
Rushit PC <rushitchandani@gmail.com>
2025-11-19 20:23:39 +0530

    manual changes

--- app.py
+++ app.py
@@ -12,15 +12,15 @@
 
 
 
-from libraries.db import db
-from route.user import user_blueprint
-from route.transactions import transaction_blueprint
+# from libraries.db import db
+# from route.user import user_blueprint
+# from route.transactions import transaction_blueprint
 from flask_cors import CORS
 
 from flask import Flask
 
 app = Flask(__name__)
-dbObj = db()
+# dbObj = db()
 app.static_folder = 'static'
 CORS(app)
 
